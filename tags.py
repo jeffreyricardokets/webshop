9a29ac422cc72f41283ccfa2898b8ed612997b86
jeffreyricardokets <93093426+jeffreyricardokets@users.noreply.github.com>
2022-04-17 14:57:16 +0200

    Add files via upload

--- tags.py
+++ tags.py
@@ -21,6 +21,12 @@ def list_products_per_tag(tag_id):
     #make an empty list
     my_list = []
     #find the tag in our database
+
+    #set a query
+    query = Tag_for_products.select(Tag_for_products,producten,Tags).join(Tags , JOIN.LEFT_OUTER).switch().join(producten, JOIN.LEFT_OUTER).switch(Tags).where(Tags.id == '2').switch(producten)
+    for item in query:
+        print(item.tag)
+    """
     query = in_stock.select().join(Tag_for_stock).where(tag_id == 1)
     #tag = tags.get(tags.id == tag_id)
     for product in query:
@@ -28,6 +34,8 @@ def list_products_per_tag(tag_id):
     #remove dublicates
     my_list = list(dict.fromkeys(my_list))
     return my_list
+    """
 
 #create_tag('test', 'lolol')
 #product_add_tag(1,3)
+list_products_per_tag(1)
